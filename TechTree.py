5fba6565cc1de1bc625cf214eefd67263e4627db
Jimmy Shamas <jeshamasIII@gmail.com>
2020-08-10 22:48:40 -0600

    Replaced abbreviated variable names.

--- TechTree.py
+++ TechTree.py
@@ -1,76 +1,93 @@
-# research level class
 from collections import namedtuple
 
-# maps level to rp cost
-cost_map = {1: 50, 2: 80, 3: 150, 4: 250, 5: 400, 6: 650, 7: 900, 8: 1150,
-            9: 1500, 10: 2000, 11: 2750, 12: 3500, 13: 4500, 14: 6000, 15: 7500
-            }
+level_to_rp_cost = {
+    1: 50, 2: 80, 3: 150, 4: 250, 5: 400, 6: 650, 7: 900, 8: 1150,
+    9: 1500, 10: 2000, 11: 2750, 12: 3500, 13: 4500, 14: 6000, 15: 7500
+}
 
-ResLevel = namedtuple('ResLevel', ['buildings', 'achievements', 'field', 'level', 'cost'])
+ResearchLevel = namedtuple(
+    'ResLevel',
+    ['buildings', 'achievements', 'field', 'level', 'rp_cost']
+)
 
-# the following assumes that the race is creative and democratic
-const = [ResLevel([], [], 'const', 1, cost_map[1]),  # removed colonyBase
-         ResLevel([], [], 'const', 2, cost_map[2]),
-         ResLevel(['automatedFactory'], [], 'const', 3, cost_map[3]),
-         ResLevel([], [], 'const', 4, cost_map[4]),
-         ResLevel(['spaceport'], [], 'const', 5, cost_map[5]),
-         ResLevel(['roboMinerPlant'], [], 'const', 6, cost_map[6]),
-         ResLevel([], [], 'const', 7, cost_map[7]),
-         ResLevel([], [], 'const', 8, cost_map[8]),
-         ResLevel(['recyclotron'], [], 'const', 9, cost_map[9]),
-         ResLevel(['roboticFactory'], [], 'const', 10, cost_map[10]),
-         ResLevel(['coreWasteDump', 'deepCoreMine'], [], 'const', 12, cost_map[12]),
-         ResLevel([], ['advancedCityPlanning'], 'const', 14, cost_map[14])
-         ]
+# Tech tree for a creative, democratic race.
+construction = [
+    ResearchLevel([], [], 'construction', 1, level_to_rp_cost[1]),  # removed colonyBase
+    ResearchLevel([], [], 'construction', 2, level_to_rp_cost[2]),
+    ResearchLevel(['automatedFactory'], [], 'construction', 3, level_to_rp_cost[3]),
+    ResearchLevel([], [], 'construction', 4, level_to_rp_cost[4]),
+    ResearchLevel(['spaceport'], [], 'construction', 5, level_to_rp_cost[5]),
+    ResearchLevel(['roboMinerPlant'], [], 'construction', 6, level_to_rp_cost[6]),
+    ResearchLevel([], [], 'construction', 7, level_to_rp_cost[7]),
+    ResearchLevel([], [], 'construction', 8, level_to_rp_cost[8]),
+    ResearchLevel(['recyclotron'], [], 'construction', 9, level_to_rp_cost[9]),
+    ResearchLevel(['roboticFactory'], [], 'construction', 10, level_to_rp_cost[10]),
+    ResearchLevel(['coreWasteDump', 'deepCoreMine'], [], 'construction', 12,
+                  level_to_rp_cost[12]),
+    ResearchLevel([], ['advancedCityPlanning'], 'construction', 14, level_to_rp_cost[14])
+]
 
-chem = [ResLevel([], [], 'chem', 1, cost_map[1]),
-        ResLevel([], [], 'chem', 4, cost_map[4]),
-        ResLevel(['pollutionProcessor'], [], 'chem', 6, cost_map[6]),
-        ResLevel(['atmosphereRenewer'], [], 'chem', 8, cost_map[8]),
-        ResLevel([], ['microliteConstruction', 'nanoDisassemblers'], 'chem', 10, cost_map[10])
-        ]
+chemistry = [
+    ResearchLevel([], [], 'chemistry', 1, level_to_rp_cost[1]),
+    ResearchLevel([], [], 'chemistry', 4, level_to_rp_cost[4]),
+    ResearchLevel(['pollutionProcessor'], [], 'chemistry', 6, level_to_rp_cost[6]),
+    ResearchLevel(['atmosphereRenewer'], [], 'chemistry', 8, level_to_rp_cost[8]),
+    ResearchLevel([], ['microliteConstruction', 'nanoDisassemblers'], 'chemistry', 10,
+                  level_to_rp_cost[10])
+]
 
-soc = [ResLevel([], [], 'soc', 3, cost_map[3]),
-       ResLevel([], [], 'soc', 6, cost_map[6]),
-       ResLevel(['stockExchange'], [], 'soc', 8, cost_map[8]),
-       ResLevel(['astroUniversity'], [], 'soc', 10, cost_map[10]),
-       ResLevel([], ['federation'], 'soc', 13, cost_map[13]),
-       ResLevel([], ['currencyExchange'], 'soc', 14, cost_map[14])
-       ]
+sociology = [
+    ResearchLevel([], [], 'sociology', 3, level_to_rp_cost[3]),
+    ResearchLevel([], [], 'sociology', 6, level_to_rp_cost[6]),
+    ResearchLevel(['stockExchange'], [], 'sociology', 8, level_to_rp_cost[8]),
+    ResearchLevel(['astroUniversity'], [], 'sociology', 10, level_to_rp_cost[10]),
+    ResearchLevel([], ['federation'], 'sociology', 13, level_to_rp_cost[13]),
+    ResearchLevel([], ['currencyExchange'], 'sociology', 14, level_to_rp_cost[14])
+]
 
-comp = [ResLevel([], [], 'comp', 1, cost_map[1]),
-        ResLevel(['researchLab'], [], 'comp', 3, cost_map[3]),
-        ResLevel([], [], 'comp', 5, cost_map[5]),
-        ResLevel(['holoSimulator', 'supercomputer'], [], 'comp', 7, cost_map[7]),
-        ResLevel([], [], 'comp', 9, cost_map[9]),
-        ResLevel(['autolab'], [], 'comp', 11, cost_map[11]),
-        ResLevel([], [], 'comp', 12, cost_map[12]),
-        ResLevel(['galacticCybernet'], ['realityNetwork'], 'comp', 13, cost_map[13]),
-        ResLevel(['pleasureDome'], [], 'comp', 14, cost_map[14])
-        ]
+computers = [
+    ResearchLevel([], [], 'computers', 1, level_to_rp_cost[1]),
+    ResearchLevel(['researchLab'], [], 'computers', 3, level_to_rp_cost[3]),
+    ResearchLevel([], [], 'computers', 5, level_to_rp_cost[5]),
+    ResearchLevel(['holoSimulator', 'supercomputer'], [], 'computers', 7,
+                  level_to_rp_cost[7]),
+    ResearchLevel([], [], 'computers', 9, level_to_rp_cost[9]),
+    ResearchLevel(['autolab'], [], 'computers', 11, level_to_rp_cost[11]),
+    ResearchLevel([], [], 'computers', 12, level_to_rp_cost[12]),
+    ResearchLevel(['galacticCybernet'], ['realityNetwork'], 'computers', 13,
+                  level_to_rp_cost[13]),
+    ResearchLevel(['pleasureDome'], [], 'computers', 14, level_to_rp_cost[14])
+]
 
 # soilEnrichment is treated as a building
-bio = [ResLevel(['biospheres', 'hydroponicFarm'], [], 'bio', 2, cost_map[2]),
-       ResLevel(['cloningCenter', 'soilEnrichment'], [], 'bio', 5, cost_map[5]),
-       ResLevel([], ['microbiotics'], 'bio', 7, cost_map[7]),
-       ResLevel(['terraforming'], [], 'bio', 8, cost_map[8]),
-       ResLevel(['subterraneanFarms', 'weatherController'], [], 'bio', 9, cost_map[9]),
-       ResLevel([], ['heightenedIntelligence'], 'bio', 11, cost_map[11]),
-       ResLevel([], ['universalAntidote'], 'bio', 13, cost_map[13]),
-       ResLevel(['gaiaTransformation'], ['biomorphicFungi'], 'bio', 15, cost_map[15])
-       ]
+biology = [
+    ResearchLevel(['biospheres', 'hydroponicFarm'], [], 'biology', 2,
+                  level_to_rp_cost[2]),
+    ResearchLevel(['cloningCenter', 'soilEnrichment'], [], 'biology', 5,
+                  level_to_rp_cost[5]),
+    ResearchLevel([], ['microbiotics'], 'biology', 7, level_to_rp_cost[7]),
+    ResearchLevel(['terraforming'], [], 'biology', 8, level_to_rp_cost[8]),
+    ResearchLevel(['subterraneanFarms', 'weatherController'], [], 'biology', 9,
+                  level_to_rp_cost[9]),
+    ResearchLevel([], ['heightenedIntelligence'], 'biology', 11, level_to_rp_cost[11]),
+    ResearchLevel([], ['universalAntidote'], 'biology', 13, level_to_rp_cost[13]),
+    ResearchLevel(['gaiaTransformation'], ['biomorphicFungi'], 'biology', 15,
+                  level_to_rp_cost[15])
+]
 
-phy = [ResLevel([], [], 'phy', 1, cost_map[1]),
-       ResLevel([], [], 'phy', 3, cost_map[3]),
-       ResLevel([], [], 'phy', 4, cost_map[4]),
-       ResLevel([], [], 'phy', 7, cost_map[7]),
-       ResLevel(['gravityGenerator'], [], 'phy', 8, cost_map[8])
-       ]
+physics = [
+    ResearchLevel([], [], 'physics', 1, level_to_rp_cost[1]),
+    ResearchLevel([], [], 'physics', 3, level_to_rp_cost[3]),
+    ResearchLevel([], [], 'physics', 4, level_to_rp_cost[4]),
+    ResearchLevel([], [], 'physics', 7, level_to_rp_cost[7]),
+    ResearchLevel(['gravityGenerator'], [], 'physics', 8, level_to_rp_cost[8])
+]
 
-ff = [ResLevel([], [], 'ff', 4, cost_map[4]),
-      ResLevel([], [], 'ff', 6, cost_map[6]),
-      ResLevel(['radiationShield'], [], 'ff', 7, cost_map[7])
-      ]
+force_fields = [
+    ResearchLevel([], [], 'force_fields', 4, level_to_rp_cost[4]),
+    ResearchLevel([], [], 'force_fields', 6, level_to_rp_cost[6]),
+    ResearchLevel(['radiationShield'], [], 'force_fields', 7, level_to_rp_cost[7])
+]
 
-tree = {'const': const, 'chem': chem, 'comp': comp,
-        'bio': bio, 'soc': soc}
+tree = {'construction': construction, 'chemistry': chemistry,
+        'computers': computers, 'biology': biology, 'sociology': sociology}
