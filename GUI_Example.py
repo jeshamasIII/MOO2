7337f61c06bdfdbce9a5c84de93acc37214e1ec6
Jimmy Shamas <jeshamasIII@gmail.com>
2020-08-18 15:32:27 -0600

    GUI example.

---
+++ GUI_Example.py
@@ -0,0 +1,25 @@
+from ColonyClass import Colony, Planet
+from GameClass import Game
+from GUI import GUI
+
+# Initialize game object
+p1 = Planet('large', 'abundant', 'normal', 'tundra')
+c1 = Colony(p1, 'Ecber II', 2, 1, 1,
+            ['hydroponicFarm', 'marineBarracks'])
+
+p2 = Planet('large', 'abundant', 'normal', 'desert')
+c2 = Colony(p2, 'Fahd II', 2, 1, 1,
+            ['marineBarracks', 'hydroponicFarm'])
+
+p3 = Planet('medium', 'abundant', 'normal', 'terran')
+c3 = Colony(p3, 'Mentar IV', 2, 3, 3,
+            ['automatedFactory', 'hydroponicFarm', 'biospheres',
+             'marineBarracks', 'soilEnrichment'])
+
+starting_tech_positions = [('construction', 6), ('chemistry', 3),
+                           ('sociology', 2), ('computers', 3), ('biology', 2)]
+
+game = Game(starting_tech_positions, [c1, c2, c3], reserve=200, stored_rp=0)
+
+# Initialize GUI
+gui = GUI(game)
